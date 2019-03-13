d717fd697bf723dd791d081d3ac2b3f6395d0327
Mottel Zirkind <shachna@gmail.com>
2019-03-13 16:31:08 -0400

    End of part 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,37 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
+import {Restaurant} from './models/restaurant';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  title = 'kosher';
-  subtitle = 'an app to show kosher things.';
-  content = 'some stuff that we fill in later';
-  // Init method
-  ngOnInit(): void {
-    this.title = 'WORD!!!';
+export class AppComponent {
+  rests: Restaurant[];
+
+  constructor() {
+    this.rests = [];
+    let temp = new Restaurant();
+    temp.name = 'Tav Cafe';
+    temp.phone = '(514) 112-3581';
+    temp.rating = 3;
+    temp.priceRange = 3;
+    temp.website = 'http://cafetav.com/';
+    this.rests.push(temp);
+
+    temp = new Restaurant();
+    temp.name = 'Pizza Pita';
+    temp.phone = '(514) 234-1111';
+    temp.rating = 4;
+    temp.priceRange = 4;
+    this.rests.push(temp);
+
+    temp = new Restaurant();
+    temp.name = 'Home';
+    temp.rating = 5;
+    temp.priceRange = 1;
+    temp.website = 'http://google.com/';
+    this.rests.push(temp);
   }
 
 }
---
+++ src/app/views/rest-card/rest-card.component.ts
@@ -0,0 +1,16 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Restaurant} from '../../models/restaurant';
+
+@Component({
+  selector: 'app-rest-card',
+  templateUrl: './rest-card.component.html',
+  styleUrls: ['./rest-card.component.scss']
+})
+export class RestCardComponent implements OnInit {
+  @Input() restaurant: Restaurant;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+}
